8f9579685ca1f361fdf4bbe6f049aebe0078f8aa
Damian-Wislee <fabricio99cv@gmail.com>
2025-06-18 12:14:48 -0500

    Implementación de splash screen personalizado en Flutter

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, '/login');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFFFFF),
+      body: Center(
+        child: Image.asset(
+          'assets/logo_splash.png', // asegúrate de que el archivo exista
+          width: 250,
+        ),
+      ),
+    );
+  }
+}
